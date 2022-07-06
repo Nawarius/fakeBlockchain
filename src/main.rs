261ca4a0a21755e993fc94c8b0400b873530bd3f
Artem Ermakov <nawarius1993@gmail.com>
2022-07-07 00:45:55 +0400

    Adds: initialize blockchain methods

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
+use chrono::prelude::*;
+
 pub struct App {
     pub blocks: Vec<Block>,
+}
+
+impl App {
+    fn new () -> Self {
+        App {
+            blocks: Vec::new()
+        }
+    }
+
+    fn genesis_block (&mut self) {
+        Block {
+            id: 0,
+            hash: String::from("0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43"),
+            prev_hash: String::from("Genesis prev hash"),
+            timestamp: Utc::now().timestamp(),
+            data: String::from("Genesis block data"),
+            nonce: 2836,
+        };
+        
+    }
 }
 
 pub struct Block {
     pub id: u64,
     pub hash: String,
     pub prev_hash: String,
-    pub timestamp: u64,
+    pub timestamp: i64,
     pub data: String,
     pub nonce: u64,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut blockchain = App::new();
+    blockchain.genesis_block();
+
+    println!("Blockchaint init!");
 }
