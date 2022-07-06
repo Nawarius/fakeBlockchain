b0ed944da85f682d4577f4f04689a7e2530930cd
Artem Ermakov <nawarius1993@gmail.com>
2022-07-07 00:28:24 +0400

    Adds: Dependencies (toml), App/Block struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+pub struct App {
+    pub blocks: Vec<Block>,
+}
+
+pub struct Block {
+    pub id: u64,
+    pub hash: String,
+    pub prev_hash: String,
+    pub timestamp: u64,
+    pub data: String,
+    pub nonce: u64,
+}
+
 fn main() {
     println!("Hello, world!");
 }
