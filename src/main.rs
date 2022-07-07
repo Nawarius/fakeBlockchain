37e5ca448fcef3912f1d6b52a1cd31fbe98c9f60
Artem Ermakov <nawarius1993@gmail.com>
2022-07-07 05:39:30 +0400

    Adds: Calc block hash, Logger, new blocks validation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use chrono::prelude::*;
-use log::{error, info, warn};
+use log::{error, info, warn, LevelFilter};
+use simplelog::{TermLogger, TerminalMode, Config};
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+
+const DIFFICULTY_PREFIX: &str = "00";
 
 pub struct App {
     pub blocks: Vec<Block>,
@@ -26,7 +31,7 @@ impl App {
 
     fn try_add_block (&mut self, block: Block) {
         let last_block = self.blocks.last().expect("At least one block must be in chain");
-        
+
         if self.is_block_valid(&block, &last_block) {
             self.blocks.push(block);
         } else {
@@ -37,9 +42,18 @@ impl App {
 
     fn is_block_valid (&self, block: &Block, prev_block: &Block) -> bool {
         if block.prev_hash != prev_block.hash {
-            warn!("Block with id {} has invalid id!", block.id);
+            warn!("Block with id {} has invalid prev hash!", block.id);
             return false
-        } 
+        } else if !hash_to_binary_string(&block.hash).starts_with(DIFFICULTY_PREFIX) {
+            warn!("Block with id: {} has invalid difficulty!", block.id);
+            return false
+        } else if block.id != prev_block.id + 1 {
+            warn!("Block with id: {} has invalid id!", block.id);
+            return false
+        } else if calc_hash(block.id, block.timestamp, &block.prev_hash, &block.data, block.nonce) != block.hash {
+            warn!("Block with id: {} has invalid hash", block.id);
+            return false
+        }
 
         true
     }
@@ -55,20 +69,49 @@ pub struct Block {
     pub nonce: u64,
 }
 
-fn hash_to_binary (hash: &String) -> String {
+fn hash_to_binary_string (hash: &String) -> String {
     let hex_hash = hex::decode(hash).expect("Can`t decode from hex");
     let mut res = String::new();
 
     for c in hex_hash { 
         res.push_str(&format!("{:b}", c)); 
     }
-
+    
     res
 }
 
+fn calc_hash (id: u64, timestamp: i64, prev_hash: &str, data: &str, nonce: u64) -> String {
+    let data = serde_json::json!({
+        "id": id,
+        "previous_hash": prev_hash,
+        "data": data,
+        "timestamp": timestamp,
+        "nonce": nonce
+    });
+
+    let mut hasher = Sha256::new();
+    hasher.update(data.to_string().as_bytes());
+    
+    hex::encode(hasher.finalize().as_slice().to_owned())
+}
+
 fn main() {
+
+    TermLogger::init(LevelFilter::Trace, Config::default(), TerminalMode::Stdout);
+    
     let mut blockchain = App::new();
     blockchain.genesis_block();
+
+    let fake_block = Block {
+        id: 1,
+        hash: calc_hash(1, 2, &blockchain.blocks[0].hash, "Data some", 1),
+        prev_hash: blockchain.blocks[0].hash.clone(),
+        timestamp: 12,
+        data: "DAta".to_string(),
+        nonce: 2,
+    };
+
+    blockchain.try_add_block(fake_block);
 
     println!("Blockchaint init!");
 }
